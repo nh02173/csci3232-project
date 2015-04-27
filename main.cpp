9d9bbf2a630cd52b5aaac678f0cdb7247ca04b4b
Nick <nh02173@georgiasouthern.edu>
2015-04-27 00:19:59 +0000

    Good build and test on FCFS

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include<cmath> 
 #include<iostream>
 #include<random>
 #include<string>
@@ -7,27 +8,29 @@ using namespace std;
 using std::string;
 using std::vector;
 
-const int MIN_PLATTERS = 1, MAX_PLATTERS = 3;
-const int MIN_TRACKS = 8, MAX_TRACKS = 32;
-const int MIN_SECTORS = 16, MAX_SECTORS = 64;
+const int MIN_TRACKS = 8, MAX_TRACKS = 256;
 const int NUM_ALGOS = 6;
 
 int main(){
     // Define disk parameters
-    int trackSelect = 0, tracks = 0, algo = 0;
+    int trackSelect = 0, startPos = 0, algo = 0;
+    string inputString;
     
     cout << "Define the number of tracks per platter [" << MIN_TRACKS << "-" << MAX_TRACKS << "]:" << endl;
     cin >> trackSelect;
-    if(trackSelect < MAX_TRACKS && trackSelect >= MIN_TRACKS){
-        tracks = trackSelect;
-    } else {
+    if(trackSelect > MAX_TRACKS || trackSelect < MIN_TRACKS){
         cerr << "Tracks was not in the specified range!" << endl;
+        return -1;
+    }
+    
+    cout << "Select a starting track within a range of 0-" << trackSelect << endl;
+    cin >> startPos;
+    if(startPos < 0 || startPos > trackSelect){
+        cerr << "Track selection outside of the specified range!" << endl;
         return -1;
     }
 
     // Collecting input data
-    string inputString;
-
     cout << "Enter a word or phrase to store on the virtual HDD:" << endl;
     cin.ignore();
     getline(cin,inputString);
@@ -37,29 +40,25 @@ int main(){
     }
 
     // Select a disk algorithm
-    int algoSelect = 0;
-    
     cout << "Select a scheduling algorithm: " << endl;
     cout << "1.FCFS  2.SSTF  3.SCAN  4.C-SCAN  5.LOOK  6.C-LOOK" << endl;
-    cin >> algoSelect;
-    if(algoSelect > 0 && algoSelect <= NUM_ALGOS){
-        algo = algoSelect;
-    } else {
-        cerr << "Invalid algorithm selection!" << endl;
+    cin >> algo;
+    if(algo < 0 || algo > NUM_ALGOS){
+        cerr << "Algorithm selection outside the specified range!" << endl;
         return -1;
     }
 
     // Use values to initialize the simulation
-    cout << "\nInitializing the disk...\n" << endl;
+    cout << "\nRandomly initializing the disk...\n" << endl;
     
     // Setup a lookup table that is the size of the data
     int lookupTable[inputString.length()];
     
-    // Setup a data structure to simulate virtual hard disk
-    char vhd[tracks];
+    // Setup data structure to simulate virtual hard disk
+    vector<char> vhd(trackSelect, 0);
     
     // Create distribution
-    uniform_int_distribution<int> distT(MIN_TRACKS,tracks);
+    uniform_int_distribution<int> distT(0, trackSelect);
 
     // Using the Mersenne Twister for randomization
     mt19937 generator;
@@ -76,28 +75,34 @@ int main(){
             // Generate positions
             genT = distT(generator);
             // Check the position
-            char checkVal = vhd[genT];
-            // Write the position
-            if(!checkVal){
+            if(vhd[genT] == 0){
                 search = false;
+                // Write the position
                 vhd[genT] = inputString[i];
                 lookupTable[i] = genT;
             }
         }
     }
     
-    cout << "\nReading data from disk...\n" << endl;
+    cout << "Reading data from disk starting at track " << startPos << "\n" << endl;
     
     // Simulate a retrieval of the data using the defined algorithm
+    int moveCount = 0;
+    
     switch(algo){
         case 1: // FCFS
+            for(i = 0; i < inputString.length(); i++){
+                cout << "Moving from position " << startPos << " to " << lookupTable[i] << " to retrieve value " << vhd[lookupTable[i]] << endl;
+                moveCount += abs(startPos - lookupTable[i]);
+                startPos = lookupTable[i];
+            }
             break;
         case 2: // SSTF
             break;
         case 3: // SCAN
             break;
         case 4: // C-SCAN
-            break:
+            break;
         case 5: // LOOK
             break;
         case 6: // C-LOOK
@@ -108,6 +113,7 @@ int main(){
     }
     
     // Report results
-
+    cout << "\nMoved a total of " << moveCount << " positions with the given algorithm " << endl;
+    
     return 0;
 }
